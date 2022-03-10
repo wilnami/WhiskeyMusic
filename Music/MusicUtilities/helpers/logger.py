06d72b16fdb591d5ca890f9e3027c49bdcce3a48
wilnami <101022475+wilnami@users.noreply.github.com>
2022-03-11 04:27:12 +0700

    terjemahan hadeh 2

--- Music/MusicUtilities/helpers/logger.py
+++ Music/MusicUtilities/helpers/logger.py
@@ -11,13 +11,13 @@ async def LOG_CHAT(message, what):
     user_name = message.from_user.first_name
     mention = "["+user_name+"](tg://user?id="+str(user_id)+")" 
     logger_text = f"""
-__**ada request banh**__
+__**wilna ada request**__
 
 **dari:** {message.chat.title} [`{message.chat.id}`]
 **nama:** {mention} hadeh
 **username:** @{message.from_user.username}
 **id:** `{message.from_user.id}`
 **link:** {chatusername} yekans
-**ini requestnya banh:** {message.text}"""
+**pesan dan kesan:** {message.text}"""
     await app.send_message(LOG_GROUP_ID, f"{logger_text}", disable_web_page_preview=True)
     
